3c5c3fc026188201a3415fe49306a0b2c9421aa9
Jessryll Quitoriano <polochan143@gmail.com>
2014-12-11 01:37:13 +0800

    1:37 12/11/2014

--- application/views/admin/assign_course_section.php
+++ application/views/admin/assign_course_section.php
@@ -4,7 +4,7 @@
 		<div class="panel-heading">
 			<a href="<?php echo base_url();?>teacher_home/teacher_assign" class="col-sm-2"><i class="fa fa-reply"></i> BACK</a>
 			<span class="col-sm-3"></span>
-			<h3 class="panel-title fa fa-clipboard"> CLASS RECORD</h3>
+			<h3 class="panel-title fa fa-clipboard"> CREATE CLASS RECORD</h3>
 		</div>
 
 		<div class="panel-body">
