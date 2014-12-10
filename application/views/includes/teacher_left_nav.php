824ebc53a97ff2c9025e4bab3f873828ae1fd81e
Jessryll Quitoriano <polochan143@gmail.com>
2014-12-10 19:22:13 +0800

    7:22 12/10/2014

--- application/views/includes/teacher_left_nav.php
+++ application/views/includes/teacher_left_nav.php
@@ -1,9 +1,13 @@
-<br><div class="col-sm-offset-1">
-<center><div class="list-group col-sm-2">
-  <a href="#" class="list-group-item">PROFILE</a>
-  <a href="#" class="list-group-item">QUESTIONNAIRES</a>
-  <a href="#" class="list-group-item">TEACHER ASSIGN</a>
-  <a href="#" class="list-group-item">SECTION</a>
-</div>
-</center>	
+<br>
+<div class="col-sm-offset-1">
+	<div class="list-group col-sm-2">
+  		<a href="#" class="list-group-item">PROFILE</a>
+  		<a href="#" class="list-group-item">QUESTIONNAIRES</a>
+  		<a href="#" class="list-group-item">QUESTION BANK</a>
+  		<a href="#" class="list-group-item">SECTION</a>
+  		<a href="#" class="list-group-item">GENERATE EXAM</a>
+  		<a href="#" class="list-group-item">STUDENT LIST</a>
+  		<a href="#" class="list-group-item">STUDENT GRADES</a>
+  		<a href="#" class="list-group-item">REPORTS</a>
+	</div>
 </div>
