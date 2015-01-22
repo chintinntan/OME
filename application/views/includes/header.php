005194b634d42be5fe2ad4207830f57e73a3b677
Jessryll Quitoriano <polochan143@gmail.com>
2015-01-22 10:26:36 +0800

    10:26 1/22/2015

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -8,6 +8,8 @@
     		echo $title;
     	?>
     </title>
+
+    <link rel="icon" href="<?php echo base_url('assets/image/uic-logo.ico');?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-theme.min.css');?>">
--- application/views/includes/teacher_left_nav.php
+++ application/views/includes/teacher_left_nav.php
@@ -4,7 +4,7 @@
       	<a href="<?php echo base_url(); ?>teacher_home/view_student_list" class="list-group-item"><i class="fa fa-list-ol"></i> <b>STUDENT LIST</b></a>
   		<a href="<?php echo base_url(); ?>question_bank" class="list-group-item"><i class="fa fa-database"></i> <b>QUESTION BANK</b></a>
   		<a href="<?php echo base_url(); ?>teacher_home/generate_exam_page" class="list-group-item"><i class="fa fa-cogs"></i> <b>GENERATE EXAM</b></a>
-  		<a href="#" class="list-group-item"><i class="fa fa-tachometer"></i> <b>STUDENT GRADES</b></a>
+  		<a href="#" class="list-group-item"><i class="fa fa-tachometer"></i> <b>STATISTIC</b></a>
   		<a href="#" class="list-group-item"><i class="fa fa-bar-chart"></i> <b>REPORTS</b></a>
       	<a href="<?php echo base_url(); ?>logout" class="list-group-item"><i class="fa fa-sign-out"></i> <b>LOGOUT</b></a>
 	</div>
