b149707256ad1cca6af6ec7285a7778ae578a2af
Jessryll Quitoriano <polochan143@gmail.com>
2015-01-22 14:30:37 +0800

    2:30 1/22/2015

---
+++ application/views/statistic/view.php
@@ -0,0 +1,31 @@
+<div class="col-md-10">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<span class="col-md-5"></span>
+			<h3 class="panel-title">STATISTIC</h3>
+		</div>
+		<div class="panel-body">
+			<div>
+				<div class="table table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>TITLE EXAM</th>
+								<th>DATE</th>
+								<th>OPTION</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>title exam</td>
+								<td>date of exam schedule</td>
+								<td><a href="<?php echo base_url();?>teacher_home/view_statistic_result_page">VIEW</a></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+
---
+++ application/views/statistic/view_statistic.php
@@ -0,0 +1,26 @@
+<div class="col-md-10">
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<span class="col-md-5"></span>
+			<h3 class="panel-title">STATISTIC</h3>
+		</div>
+		<div class="panel-body">
+			<div>
+				<div class="table table-responsive">
+					<table class="table">
+						<thead>
+							<tr>
+								<th>question id</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
