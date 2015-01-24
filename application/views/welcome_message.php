093c686b15f90e2fc33f1e56b609b5b077f02a23
Jessryll Quitoriano <polochan143@gmail.com>
2015-01-24 17:06:12 +0800

    5:06 1/24/2015

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,6 @@
 <br>
 <div class="col-md-4 col-md-offset-4">
-  <div class="panel panel-default">
+  <div class="panel panel-warning">
     
       <div class="panel-heading">
         <h3 class="panel-title fa fa-sign-in"> LOGIN</h3>
